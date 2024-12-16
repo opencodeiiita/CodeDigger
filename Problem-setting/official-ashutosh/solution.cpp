5150733293df0a719197bfa3a881dbee74c8836e
Ashutosh Kumar <officialashutosh0@gmail.com>
2024-12-17 00:13:06 +0530

    Added 1st poblem

---
+++ Problem-setting/official-ashutosh/solution.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+ 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n*m - 1) << endl;
+}
+ 
+signed main() {
+    
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        solve();       
+    }
+    return 0;
+}
+
